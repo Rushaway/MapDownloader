a5dd0d78be522055ecf4e3ea0f007d86058eb184
Rushaway <rushaway@hotmail.fr>
2025-04-06 14:18:43 +0200

    Update Global.cs

--- MapDownloader/Global.cs
+++ MapDownloader/Global.cs
@@ -8,7 +8,6 @@ namespace MapDownloader
 	{
 		public static string version = "2.1";
 		public static string fastdlUrl = "";
-		public static string maplistUrl = "";
 		public static string appID = "";
 		public static string mapsDirectory = "";
 	}
